2b6327b6571b8ad9dd10c99008cb624e80349bf0
PacketYaravoi <rik1810huragan@gmail.com>
2025-04-16 19:50:09 +1100

    сейв на задаче 10. Первая часть выполненна.Переходик к 2й части.

---
+++ src/dataOutput/task10/main.java
@@ -0,0 +1,17 @@
+package dataOutput.task10;
+
+public class main {
+    public static void main(String[] args) {
+
+        /**
+         * Задача 10
+         * Объявите две переменные x и y типа int и присвойте им значения.
+         * Выведите на экран x - y = разность этих двух чисел.
+         */
+
+        int x = 10;
+        int y = 5;
+        int result = x -y;
+        System.out.printf("разность чисел %d - %d = %d%n",x,y,result);
+    }
+}
---
+++ src/dataOutput/task8/main.java
@@ -0,0 +1,14 @@
+package dataOutput.task8;
+
+public class main {
+    public static void main(String[] args) {
+
+        /**
+         * Задача 8
+         * Объявите переменную дробь типа float и присвойте ей значение дробного числа. Выведите на экран это число с двумя знаками после запятой.
+         */
+
+        float numberfloat = 3.1415f;
+        System.out.printf("Число pi = %.2f%n", numberfloat);
+    }
+}
