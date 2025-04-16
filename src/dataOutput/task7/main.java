e9fd180c3be3a82c1cddfe37e38850781ba0cf9f
PacketYaravoi <rik1810huragan@gmail.com>
2025-04-16 19:35:13 +1100

    сейв на задаче 7

---
+++ src/dataOutput/task7/main.java
@@ -0,0 +1,15 @@
+package dataOutput.task7;
+
+public class main {
+    public static void main(String[] args) {
+
+        /**
+         * Задача 7
+         * Объявите переменную bigNumber типа long и присвойте ей большое число. Выведите на экран это число.
+         */
+
+        long bigNumber = 2700000000000000L;
+        System.out.printf("Очень большое число- %d , с ним справиться только тип long", bigNumber);
+
+    }
+}
