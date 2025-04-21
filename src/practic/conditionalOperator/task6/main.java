046387c18adfee37b6cdd7ea105d0ee8cf0cd93d
PacketYaravoi <rik1810huragan@gmail.com>
2025-04-21 17:53:46 +1100

    сейв на 7й задаче послднего блока (условные операторы)

--- src/practic/conditionalOperator/task6/main.java
+++ src/practic/conditionalOperator/task6/main.java
@@ -1,4 +1,29 @@
 package practic.conditionalOperator.task6;
 
+import java.util.Scanner;
+
 public class main {
+    public static void main(String[] args) {
+        /**
+         * Задача 6.
+         * Вместимость поезда — 2500 человек. Поезд рассчитан на 344 сидячих мест, все остальные — стоячие.
+         * С помощью условного оператора и конструкции else if напишите программу, которая выводит в консоль сообщение
+         * о том, есть ли место в вагоне, сидячее или стоячее, или поезд уже полностью забит.
+         */
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите текущее колличество людей в поезде:");
+        int peaple = scanner.nextInt();
+
+        if(peaple >= 344 && peaple <=2500) {
+            System.out.printf("Места еще есть! Вы можете приобрести только стоячее место!");
+        }else if ((peaple <= 344 && peaple >=0)) {
+            System.out.printf("Места еще есть! Вы можете приобрести седячее место!");
+        }
+        else {
+            System.out.println("Вы вышли из диапазона колличества вместимости поезда");
+        }
+    }
 }
--- src/practic/conditionalOperator/task7/main.java
+++ src/practic/conditionalOperator/task7/main.java
@@ -1,4 +1,31 @@
 package practic.conditionalOperator.task7;
 
+import java.util.Scanner;
+
 public class main {
+    public static void main(String[] args) {
+
+        /**
+         *Задача 7.
+         * Даны 2 числа. С помощью условного оператора выведите значение большего.
+         */
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("!!!Введите два числа!!!");
+
+        System.out.print("Введите первое число: ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Введите второе число: ");
+        int num2 = scanner.nextInt();
+
+        if (num1 > num2){
+            System.out.printf("Первое число %d, больше чем второе число %d%n",num1, num2);
+        }else {
+            System.out.printf("Второе число %d, больше чем первое число %d%n", num2, num1);
+        }
+
+
+    }
+
 }
