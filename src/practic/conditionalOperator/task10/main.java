e7280181df33370470b1db047b111eeee2965bed
PacketYaravoi <rik1810huragan@gmail.com>
2025-04-22 12:05:20 +1100

    Сейв на 11й задаче

--- src/practic/conditionalOperator/task10/main.java
+++ src/practic/conditionalOperator/task10/main.java
@@ -1,4 +1,42 @@
 package practic.conditionalOperator.task10;
 
+import java.util.Scanner;
+
 public class main {
+    public static void main(String[] args) {
+
+        /**
+         * Задача 10.
+         * У банка появилось мобильное приложение. Когда пользователь заходит на сайт с телефона, ему предлагается скачать приложение с учетом ОС и годом его создания.
+         * Ваша задача — написать программу, которая выдает соответствующее сообщение клиенту при наличии двух условий.
+         * Если год выпуска ранее 2020 года, то к сообщению об установке нужно добавить информацию о лайт версии:
+         * Для iOS оно будет звучать так: «Скачайте лайт версию приложения для iOS по ссылке».
+         * Для Android: «Скачайте лайт версию приложения для Android по ссылке».
+         * Для пользователей телефонов 2020 года выпуска и позже нужно вывести обычное предложение об установке приложения.
+         * Для года создания телефона используйте переменную clientPhoneYear, в которой необходимо указать 2020 год. Используйте не больше двух вложенностей в условии.
+         */
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.printf("!!!Введите вашу ОС!!!%nГде 1 - Android, а 0 - IOS%n" );
+        int clientOperationSystem = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("!!!Введите год выпуска вашего устройства:" );
+        int ageGaget = scanner.nextInt();
+        scanner.nextLine();
+
+        if (clientOperationSystem == 0 && ageGaget < 2020){
+            System.out.println("Скачайте лайт версию приложения для iOS по ссылке");
+
+        } else if (clientOperationSystem == 1 && ageGaget < 2020) {
+            System.out.println("Скачайте лайт версию приложения для Android по ссылке");
+
+        } else if (clientOperationSystem == 0 && ageGaget >= 2020) {
+            System.out.println("Установка приложения для IOS по ссылке");
+
+        } else {
+            System.out.println("Установка приложения для Android по ссылке");
+        }
+    }
 }
--- src/practic/conditionalOperator/task11/main.java
+++ src/practic/conditionalOperator/task11/main.java
@@ -1,4 +1,38 @@
 package practic.conditionalOperator.task11;
 
+import java.util.Scanner;
+
 public class main {
+    public static void main(String[] args) {
+
+        /**
+         * Задача 11.
+         * Напишите программу, которая определяет, является ли год високосным или нет.
+         * Переменную года назовите year, в которую можно подставить значение интересующего нас года.
+         * Например, 2021. Программа должна определять, високосный год или нет, и выводить соответствующее сообщение:
+         * « …. год является високосным» или «... год не является високосным».
+         * Високосным является каждый четвертый год, но не является каждый сотый.
+         * Также високосным является каждый четырехсотый год.
+         */
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Введите год для проверки на высокосность:");
+
+        int year = in.nextInt();
+
+        boolean isEveryFour = (year % 4) == 0;
+        boolean isEveryHundred = (year % 100) == 0;
+        boolean isEverFourHungry = (year % 400) == 0;
+
+        if(isEveryFour && !isEveryHundred || isEverFourHungry){
+            System.out.println(year + " - год высокосный");
+        }
+        else{
+            System.out.println(year + " - не высокосный год");
+        }
+    }
+}
+
+    }
 }
