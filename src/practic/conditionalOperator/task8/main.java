8e0f70aa539e738b7136abf2a8500065effb8159
PacketYaravoi <rik1810huragan@gmail.com>
2025-04-22 11:39:27 +1100

    Сейв на 9й задаче

--- src/practic/conditionalOperator/task8/main.java
+++ src/practic/conditionalOperator/task8/main.java
@@ -1,4 +1,51 @@
 package practic.conditionalOperator.task8;
 
+import java.util.Scanner;
+
 public class main {
+    public static void main(String[] args) {
+
+        /**
+         * Задача 8.
+         * Даны 3 числа. С помощью условного оператора выведите значение меньшего.
+         */
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("!!!Введите три числа!!!");
+
+        System.out.print("Введите первое число: ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Введите второе число: ");
+        int num2 = scanner.nextInt();
+
+        System.out.print("Введите третье число: ");
+        int num3 = scanner.nextInt();
+
+        if (num1 < num2 && num1 < num3){
+            System.out.printf("Первое число %d, меньше чем второе число %d%n, и меньше чем третье %d",num1, num2, num3);
+        }
+
+        else if (num2 < num1 && num2 < num3){
+            System.out.printf("Второе число %d, меньше чем первое число %d%n, и меньше чем третье %d", num2, num1, num3);
+        }
+
+        else if (num1 == num2 && num2 < num3){
+            System.out.printf("1е и 2е числа равны - %d они же наименьшие");
+        }
+
+        else if (num1 == num2 && num2 > num3) {
+            System.out.printf("1е и 2е числа равны - %d, наименьшее число 3е - %d", num1, num3);
+        }
+        else if (num2 == num3 && num2 < num3) {
+            System.out.printf("1е и 2е числа равны - %d они же наименьшие");
+        }
+        else if (num1 == num2 && num2 == num3) {
+            System.out.println("все равны");
+        } else {
+            System.out.println("Некоректные данные");
+        }
+    }
 }
--- src/practic/conditionalOperator/task9/main.java
+++ src/practic/conditionalOperator/task9/main.java
@@ -1,4 +1,37 @@
 package practic.conditionalOperator.task9;
 
+import java.util.Scanner;
+
 public class main {
+    public static void main(String[] args) {
+
+        /**
+         * Задача 9.
+         * У банка появилось мобильное приложение. Когда пользователь заходит на сайт с телефона,
+         * ему предлагается скачать приложение с учетом ОС.
+         * Напишите программу, которая определяет, чем пользуется клиент (iOS или Android),
+         * и выдает соответствующее сообщение:
+         * Для iOS — «Установите версию приложения для iOS».
+         * Для Android — «Установите версию приложения для Android».
+         * Объявите переменную clientOperationSystem, которая равна 0 или 1 (0 — iOS, 1 — Android).
+         */
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.printf("!!!Введите вашу ОС!!!%nГде 1 - Android, а 0 - IOS%n" );
+        int clientOperationSystem = scanner.nextInt();
+        scanner.nextLine();
+
+        switch (clientOperationSystem){
+            case 1:
+                System.out.println("Установите версию для Android");
+                break;
+            case 0:
+                System.out.println("Установите версию для IOS");
+                break;
+        }
+
+
+    }
+
 }
